0a0c15528d94b1196b989d5886dd6b824275b601
lengocbao <lengocbaovlth1513010@gmail.com>
2019-10-17 17:11:06 +0700

    initial commit

---
+++ config/admin.js
@@ -0,0 +1,9 @@
+// config used by dashboard client side only
+module.exports = {
+	// dashboard UI language
+	language: 'en',
+	apiBaseUrl: 'http://localhost:3001/api/v1',
+	baseUrl:'http://localhost:3000/api',
+	apiWebSocketUrl: 'ws://localhost:3001',
+	developerMode: true
+};
